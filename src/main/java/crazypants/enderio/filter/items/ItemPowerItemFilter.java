912cdae38a0cfd004727bdc1c782e03f9297c21a
EpicSquid <epicsquid315@gmail.com>
2017-10-18 01:28:30 +1100

    Began Update Power Item Filter

--- src/main/java/crazypants/enderio/filter/items/ItemPowerItemFilter.java
+++ src/main/java/crazypants/enderio/filter/items/ItemPowerItemFilter.java
@@ -3,13 +3,15 @@ package crazypants.enderio.filter.items;
 import com.enderio.core.api.client.gui.IResourceTooltipProvider;
 
 import crazypants.enderio.EnderIOTab;
-import crazypants.enderio.ModObject;
 import crazypants.enderio.filter.IItemFilter;
 import crazypants.enderio.filter.IItemFilterUpgrade;
 import crazypants.enderio.filter.filters.PowerItemFilter;
+import crazypants.enderio.init.IModObject;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import javax.annotation.Nonnull;
 
 /**
  *
@@ -17,17 +19,14 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
  */
 public class ItemPowerItemFilter extends Item implements IItemFilterUpgrade, IResourceTooltipProvider {
 
-  public static ItemPowerItemFilter create() {
-    ItemPowerItemFilter result = new ItemPowerItemFilter();
-    result.init();
-    return result;
+  public static ItemPowerItemFilter create(@Nonnull IModObject modObject) {
+    return new ItemPowerItemFilter(modObject);
   }
 
-  protected ItemPowerItemFilter() {
+  protected ItemPowerItemFilter(@Nonnull IModObject modObject) {
     setCreativeTab(EnderIOTab.tabEnderIOItems);
-    setUnlocalizedName(ModObject.itemPowerItemFilter.getUnlocalisedName());
-    setRegistryName(ModObject.itemPowerItemFilter.getUnlocalisedName());
-    setHasSubtypes(true);
+  modObject.apply(this);
+  setHasSubtypes(true);
     setMaxDamage(0);
     setMaxStackSize(64);
   }
@@ -37,7 +36,7 @@ public class ItemPowerItemFilter extends Item implements IItemFilterUpgrade, IRe
   }
 
   @Override
-  public IItemFilter createFilterFromStack(ItemStack stack) {
+  public IItemFilter createFilterFromStack(@Nonnull ItemStack stack) {
     IItemFilter filter = new PowerItemFilter();
     if(stack.getTagCompound() != null && stack.getTagCompound().hasKey("filter")) {
       filter.readFromNBT(stack.getTagCompound().getCompoundTag("filter"));
@@ -52,12 +51,12 @@ public class ItemPowerItemFilter extends Item implements IItemFilterUpgrade, IRe
 //  }
 
   @Override
-  public String getUnlocalizedName(ItemStack stack) {
+  public String getUnlocalizedName(@Nonnull ItemStack stack) {
     return getUnlocalizedName();
   }
 
   @Override
-  public String getUnlocalizedNameForTooltip(ItemStack stack) {
+  public String getUnlocalizedNameForTooltip(@Nonnull ItemStack stack) {
     return getUnlocalizedName();
   }
 
